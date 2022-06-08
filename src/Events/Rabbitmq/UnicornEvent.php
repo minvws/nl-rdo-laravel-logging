8c1d984e29f3131ea0a470103e4bce585f6ce488
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-06-08 15:36:52 +0200

    fixed unicorn event

--- src/Events/Rabbitmq/UnicornEvent.php
+++ src/Events/Rabbitmq/UnicornEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Events\Rabbitmq;
 
-use Nuwber\Events\Event\AbstractPublishableEvent;
+use RabbitEvents\Publisher\Support\AbstractPublishableEvent;
 
 class UnicornEvent extends AbstractPublishableEvent
 {
