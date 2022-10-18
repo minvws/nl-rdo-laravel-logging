f574c4df1df041c8b5a5147f2d2b552acb3ac021
Joshua Thijssen <jthijssen@noxlogic.nl>
2022-10-18 09:07:21 +0200

    Added organisation events

---
+++ src/Events/Logging/OrganisationChangeLogEvent.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MinVWS\Logging\Laravel\Events\Logging;
+
+use MinVWS\Logging\Laravel\Contracts\LoggableUser;
+
+class OrganisationChangeLogEvent extends GeneralLogEvent
+{
+    public const EVENT_CODE = '090013';
+    public const EVENT_KEY = 'organisation_changed';
+
+    public function __construct(
+        public ?LoggableUser $actor,
+        public ?LoggableUser $target,
+        public array $data = [],
+        public array $piiData = [],
+        public string $actionCode = GeneralLogEvent::AC_UPDATE,
+        public bool $allowedAdminView = false,
+        public bool $failed = false,
+        public string $source = '',
+    ) {
+        parent::__construct(
+            $actor,
+            $target,
+            $data,
+            $piiData,
+            self::EVENT_CODE,
+            $actionCode,
+            $allowedAdminView,
+            $failed,
+            $source
+        );
+    }
+}
