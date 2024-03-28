5b61adf0fb68b0bc7a93cc972823e8846051e8b2
Gerbrand Bosch <gerbrandb@gmail.com>
2024-03-28 08:50:10 +0100

    fix test

--- tests/User.php
+++ tests/User.php
@@ -8,9 +8,9 @@ use MinVWS\Logging\Laravel\Contracts\LoggableUser;
 
 class User implements LoggableUser
 {
-    public string|null $id;
-    public string|null $email;
-    public string|null $name;
-    public string|null $ggd_region;
-    public string|null $roles;
+    public string|null $id = null;
+    public string|null $email = null;
+    public string|null $name = null;
+    public string|null $ggd_region = null;
+    public string|null $roles = null;
 }
