729d368c9772c908f4b258390f6c98732ebf4fcf
Maurice Maas <maurice@egeniq.com>
2024-04-17 13:11:38 +0200

    Removing import

--- database/migrations/2023_03_28_084221_initial_application_model.php
+++ database/migrations/2023_03_28_084221_initial_application_model.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use MinVWS\DUSi\Shared\Application\Models\Connection;
 
 return new class extends Migration
 {
